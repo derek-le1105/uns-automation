b94eb6b379810091815243a70de97a517d637361
derek-le1105 <derekle1105@gmail.com>
2023-09-29 15:21:53 -0700

    Beginning Firebase implementation

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
+import { useState } from "react";
+import { storage } from "./firebase";
+import { ref, uploadBytes } from "firebase/storage";
+import { v4 } from "uuid";
+
+//TODO: import firebase.js
 
 function App() {
+  const [fileUpload, setFileUpload] = useState(null);
+  const uploadFile = () => {
+    if (fileUpload == null) return;
+
+    const fileRef = ref(storage, `files/${fileUpload.name + v4()}`);
+
+    uploadBytes(fileRef, fileUpload).then(
+      () => {
+        alert("File uploaded");
+      },
+      (error) => {
+        alert(error);
+      }
+    );
+  };
+
   return (
     <div className="App">
-      <input type="file"></input>
+      <input
+        onChange={(e) => {
+          setFileUpload(e.target.files[0]);
+        }}
+        type="file"
+      ></input>
+      <button onClick={uploadFile}>Upload</button>
     </div>
   );
 }
