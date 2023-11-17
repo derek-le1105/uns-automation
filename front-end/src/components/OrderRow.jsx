68fe90f4e22305e88177d699eb60864927082cce
derek-le1105 <derekle1105@gmail.com>
2023-11-17 14:10:34 -0800

    Collapsible orders implemented

---
+++ front-end/src/components/OrderRow.jsx
@@ -0,0 +1,75 @@
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableCell,
+  TableRow,
+  Box,
+} from "@mui/material";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+
+import { useState } from "react";
+
+const OrderRow = (props) => {
+  const { order } = props;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <TableRow sx={{ "& > *": { borderBottom: "none" } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {order.order_name}
+        </TableCell>
+        <TableCell align="left">{order.id}</TableCell>
+        <TableCell align="left">{order.customer.first_name}</TableCell>
+      </TableRow>
+
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Quantity</TableCell>
+                    <TableCell>Plant</TableCell>
+                    <TableCell align="left">SKU</TableCell>
+                    <TableCell align="left">Vendor</TableCell>
+                    <TableCell align="left">Barcode</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {order.items.map((item) => (
+                    <TableRow key={item.title}>
+                      <TableCell component="th" scope="row">
+                        {item.quantity}
+                      </TableCell>
+                      <TableCell>{item.title}</TableCell>
+                      <TableCell align="left">{item.sku}</TableCell>
+                      <TableCell align="left">{item.vendor}</TableCell>
+                      <TableCell align="left">{item.barcode}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </>
+  );
+};
+
+export default OrderRow;
--- front-end/src/components/OrdersListing.jsx
+++ front-end/src/components/OrdersListing.jsx
@@ -1,48 +1,36 @@
-import CustomizedTable from "./CustomizedTable";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import { useState } from "react";
+import OrderRow from "./OrderRow";
 
 const OrdersListing = ({ orders }) => {
   return (
-    <div className="order-listings">
-      {orders && orders.map((order, idx) => {
-        return(
-          <>
-            <div className={`customer ${order.customer}`}>{order.customer} {idx+1}</div>
-            <CustomizedTable order={order}/>
-          </>
-          
-          /*<div className={`order ${idx+1}`} key={idx}>
-            <div className={`customer ${order.customer}`}>{order.customer} {idx+1}</div>
-            <TableContainer component={Paper}>
-              <Table aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Quantity</TableCell>
-                    <TableCell align="left">Plant Name</TableCell>
-                    <TableCell align="left">Barcode</TableCell>
-                    <TableCell align="left">Fulfillment Number</TableCell>
-                    <TableCell align="left">Vendor</TableCell>
-                    <TableCell align="left">SKU</TableCell>
-                    <TableCell align="left">Customer Code</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                {order.items.map((item, key) => {
-                    return (<TableRow key={key}>
-                      <TableCell>{item.quantity}</TableCell>
-                      <TableCell align="left">{item.title}</TableCell>
-                      <TableCell align="left">{item.barcode}</TableCell>
-                      <TableCell align="left">{item.fulfillment_number}</TableCell>
-                      <TableCell align="left">{item.vendor}</TableCell>
-                      <TableCell align="left">{item.sku}</TableCell>
-                      <TableCell align="left">{item.customer_code}</TableCell>
-                    </TableRow>)
-                  })}
-                </TableBody>
-              </Table>
-            </TableContainer>       
-                </div>*/)
-        })}
-    </div>
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell>Customer</TableCell>
+            <TableCell align="left">Fulfillment Number</TableCell>
+            <TableCell align="left">Store Name</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {orders &&
+            orders.map((order) => {
+              return <OrderRow key={order.order_name} order={order} />;
+            })}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
