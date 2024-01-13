7220df4ea9ab6713163530ac6fbfd0179f3dad28
Derek Le <derekle1105@gmail.com>
2024-01-13 10:56:47 -0800

    Refactoring app to use Supabase

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,14 +3,28 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import OrdersListing from "./components/wholesale/OrdersListing";
-import { useAuthContext } from "./hooks/useAuthContext";
+import { useState, useEffect } from "react";
 
 import { SnackbarProvider } from "notistack";
 
+import { supabase } from "./supabaseClient";
+
+// import { storage, auth } from "./firebase";
+// import { onAuthStateChanged } from "firebase/auth";
 //TODO: import firebase.js
 
 function App() {
-  const { user } = useAuthContext();
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
 
   return (
     <SnackbarProvider autoHideDuration={3000}>
@@ -19,25 +33,29 @@ function App() {
           <Routes>
             <Route
               path="/"
-              element={user ? <Navigate to="/wholesale" /> : <Login />}
+              element={session ? <Navigate to="/wholesale" /> : <Login />}
             />
             <Route
               path="/home"
-              element={user ? <Home /> : <Navigate to="/" />}
+              element={session ? <Home /> : <Navigate to="/" />}
             ></Route>
             <Route
               path="/wholesale"
               element={
-                user ? <Home link={<OrdersListing />} /> : <Navigate to="/" />
+                session ? (
+                  <Home link={<OrdersListing />} />
+                ) : (
+                  <Navigate to="/" />
+                )
               }
             ></Route>
             <Route
               path="/warehouse"
-              element={user ? <Home /> : <Navigate to="/" />}
+              element={session ? <Home /> : <Navigate to="/" />}
             ></Route>
             <Route
               path="/history"
-              element={user ? <Home /> : <Navigate to="/" />}
+              element={session ? <Home /> : <Navigate to="/" />}
             ></Route>
           </Routes>
         </BrowserRouter>
