3e5e3d253795ec2e19fdfa17fd0fbef16531eadc
Derek Le <derekle1105@gmail.com>
2024-06-14 13:55:28 -0700

    Strictly allowing only xlsx files to be read

--- front-end/src/components/FileUpload.jsx
+++ front-end/src/components/FileUpload.jsx
@@ -40,7 +40,7 @@ const FileUpload = ({ fileHandler, componentString = "Upload File" }) => {
       }}
     >
       {filename}
-      <VisuallyHiddenInput type="file" accept=".xlsx, .csv, .xls" />
+      <VisuallyHiddenInput type="file" accept=".xlsx" />
     </Button>
   );
 };
