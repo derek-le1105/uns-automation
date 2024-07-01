816156d6cb033521c2b4374021164ff4a5bb467b
Derek Le <derekle1105@gmail.com>
2024-07-01 08:53:36 -0700

    fixed wca

--- back-end/routes/wca.js
+++ back-end/routes/wca.js
@@ -26,56 +26,48 @@ router.post("/statuses", async (req, res) => {
 });
 
 router.post("/products", async (req, res) => {
-  let wca_stocklist_codes = req.body;
+  let products = req.body;
   try {
-    let { productUpdateList } = await prepareShopifyImport(
-      wca_stocklist_codes,
-      filterString,
-      "acw"
-    );
-
     let response = "";
-    if (productUpdateList.length !== 0) {
+
+    if (products.length !== 0) {
       let polling = await importBulkData(
-        productUpdateList,
-        "wcatest",
+        products,
+        "wcaimport",
         productUpdateString
       );
       response = polling
         ? "Successfully updated products"
         : "Something went wrong";
     } else response = "No products to update";
-
     return res.status(200).json(response);
   } catch (error) {
-    console.log(error);
+    console.log(error.lineNumber);
     return res.status(404).json(error);
   }
 });
 router.post("/variants", async (req, res) => {
-  let wca_stocklist_codes = req.body;
+  let variants = req.body;
   try {
-    let { productUpdateVariantList } = await prepareShopifyImport(
-      wca_stocklist_codes,
-      filterString,
-      "acw"
-    );
-
     let response = "";
-    if (productUpdateVariantList.length !== 0) {
+
+    if (variants.length !== 0) {
+      let formatted_variants = variants.map((variant) => ({
+        id: variant.id,
+        inventoryPolicy: variant.inventoryPolicy,
+      }));
       let polling = await importBulkData(
-        productUpdateVariantList,
-        "wcatest",
+        formatted_variants,
+        "wcavariantimport",
         variantUpdateString
       );
       response = polling
         ? "Successfully updated variants"
         : "Something went wrong";
     } else response = "No variants to update";
-
     return res.status(200).json(response);
   } catch (error) {
-    console.log(error);
+    console.log(error.lineNumber);
     return res.status(404).json(error);
   }
 });
